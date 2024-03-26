af4cc9e56e2488bde61c93b9614a5d984479f1c2
daryapatsko <daryapatsko95@gmail.com>
2024-03-26 12:02:52 +0300

    feat: slider swiper сщттусешщт

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 30,
+    breakpoints: {
+        1024: {
+            slidesPerView: 3,
+        },
+        600: {
+          slidesPerView: 2,
+        },
+        450: {
+          slidesPerView: 1,
+        },
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
+
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("burger").addEventListener("click",function(){
         document.querySelector(".header").classList.toggle("open"),
