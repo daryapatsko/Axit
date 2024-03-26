cbfb09a2fa4cc3e5928bf0d8343d48b06b5f50dc
daryapatsko <daryapatsko95@gmail.com>
2024-03-26 11:11:33 +0300

    feat: change stylr for sections

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-new Swiper('.swiper-container',{
-    spaceBetween:30,
-    pagination:{
-        el:'.swiper-pagination',
-        clickable:true,
-    },
-});
-
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("burger").addEventListener("click",function(){
         document.querySelector(".header").classList.toggle("open"),
